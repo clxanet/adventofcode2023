7a563a2e019e570f355c5b46e15c91c42331df09
clxa.net <clxanet@pm.me>
2023-12-02 18:29:21 +0200

    day2 refactor vec<(i32, Color)> into r,g,b,

--- src/day2.rs
+++ src/day2.rs
@@ -10,10 +10,14 @@ impl Day2 {
     }
 
     pub fn sum(&self) -> i32 {
-        const LIMITS: &[(i32, Color)] = &[(12, Color::Red), (13, Color::Green), (14, Color::Blue)];
+        const LIMITS: Game = Game {
+            red: 12,
+            green: 13,
+            blue: 14,
+        };
         self.games
             .iter()
-            .filter(|line| line.is_possible(LIMITS))
+            .filter(|line| line.is_possible(&LIMITS))
             .map(|line| line.game_id)
             .sum()
     }
@@ -33,80 +37,39 @@ impl Line {
         Self { game_id, games }
     }
 
-    fn is_possible(&self, limits: &[(i32, Color)]) -> bool {
+    fn is_possible(&self, limits: &Game) -> bool {
         self.games.iter().all(|game| game.is_possile(limits))
     }
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, Default, PartialEq, PartialOrd)]
 struct Game {
-    cubes: Vec<(i32, Color)>,
+    red: i32,
+    blue: i32,
+    green: i32,
 }
 
 impl Game {
     fn parse(s: &str) -> Self {
+        let mut red = 0;
+        let mut blue = 0;
+        let mut green = 0;
         let s = s.trim();
-        let cubes = s.split(',').map(Color::parse).collect();
-        Self { cubes }
-    }
-
-    fn is_possile(&self, limits: &[(i32, Color)]) -> bool {
-        let mut target = [0; Color::COUNT];
-        let mut this = [0; Color::COUNT];
-        for limit in limits {
-            target[limit.1.to_index()] = limit.0;
-        }
-        for cube in self.cubes.iter() {
-            this[cube.1.to_index()] = cube.0;
-        }
-
-        for (a, b) in target.iter().copied().zip(this.iter().copied()) {
-            if a < b {
-                return false;
+        for cube in s.split(',') {
+            let (num, color) = cube.trim().split_once(' ').unwrap();
+            let num = num.parse::<i32>().unwrap();
+            match color {
+                "red" => red = num,
+                "green" => green = num,
+                "blue" => blue = num,
+                color => panic!("unknown color: {color}"),
             }
         }
-        true
-    }
-}
-
-#[derive(Debug, PartialEq)]
-enum Color {
-    Red,
-    Green,
-    Blue,
-}
-
-impl Color {
-    fn parse(s: &str) -> (i32, Self) {
-        let s = s.trim();
-        let (num, color) = s.split_once(' ').unwrap();
-        (
-            num.trim().parse().unwrap(),
-            match color.trim() {
-                "red" => Self::Red,
-                "blue" => Self::Blue,
-                "green" => Self::Green,
-                color => panic!("unknown color: {color}"),
-            },
-        )
+        Self { red, blue, green }
     }
 
-    const COUNT: usize = 3;
-    fn to_index(&self) -> usize {
-        match self {
-            Color::Red => 0,
-            Color::Green => 1,
-            Color::Blue => 2,
-        }
-    }
-
-    fn from_index(i: usize) -> Self {
-        match i {
-            0 => Color::Red,
-            1 => Color::Green,
-            2 => Color::Blue,
-            _ => panic!(),
-        }
+    fn is_possile(&self, limits: &Self) -> bool {
+        self <= limits
     }
 }
 
@@ -122,13 +85,19 @@ mod tests {
                 game_id: 1,
                 games: vec![
                     Game {
-                        cubes: vec![(3, Color::Blue), (4, Color::Red)]
+                        blue: 3,
+                        red: 4,
+                        green: 0,
                     },
                     Game {
-                        cubes: vec![(1, Color::Red), (2, Color::Green), (6, Color::Blue)]
+                        red: 1,
+                        green: 2,
+                        blue: 6,
                     },
                     Game {
-                        cubes: vec![(2, Color::Green)]
+                        green: 2,
+                        red: 0,
+                        blue: 0,
                     }
                 ]
             }
@@ -138,21 +107,36 @@ mod tests {
     #[test]
     fn possible_games() {
         assert!(Game {
-            cubes: vec![(2, Color::Blue)]
+            blue: 2,
+            ..Default::default()
         }
-        .is_possile(&[(2, Color::Blue)]));
+        .is_possile(&Game {
+            blue: 2,
+            ..Default::default()
+        }));
+
         assert!(Game {
-            cubes: vec![(1, Color::Blue)]
+            blue: 1,
+            ..Default::default()
         }
-        .is_possile(&[(2, Color::Blue)]));
-        assert!(!Game {
-            cubes: vec![(2, Color::Blue)]
-        }
-        .is_possile(&[(1, Color::Blue)]));
+        .is_possile(&Game {
+            blue: 2,
+            ..Default::default()
+        }));
 
         assert!(!Game {
-            cubes: vec![(1, Color::Blue)]
+            blue: 2,
+            ..Default::default()
         }
-        .is_possile(&[]));
+        .is_possile(&Game {
+            blue: 1,
+            ..Default::default()
+        }));
+
+        assert!(!Game {
+            blue: 1,
+            ..Default::default()
+        }
+        .is_possile(&Game::default()));
     }
 }
