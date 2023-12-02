a36bd7500d8c868d699543ff605f6f434a84929b
clxa.net <clxanet@pm.me>
2023-12-02 11:08:32 +0200

    almost solved day1: one red test

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,79 @@ struct Args {
 
 impl Args {
     fn execute(self) {
-        println!("{self:?}");
+        match self.day {
+            1 => println!("{:?}", Day1::solve(&self.input())),
+            day => unimplemented!("the solution for day {} is missing", day),
+        }
+    }
+
+    fn input(&self) -> Vec<String> {
+        let mut lines = Vec::new();
+        if let Some(ref single) = self.single {
+            lines.push(single.clone())
+        }
+        assert!(
+            !lines.is_empty(),
+            "please provide a single line input or an input file path"
+        );
+        lines
+    }
+}
+
+#[derive(Debug, PartialEq)]
+struct Day1 {
+    solutions: Vec<(Option<char>, Option<char>)>,
+}
+
+impl Day1 {
+    fn solve<S: AsRef<str>>(lines: &[S]) -> Self {
+        let mut solutions = Vec::with_capacity(lines.len());
+        for line in lines {
+            let mut first = None;
+            let mut last = None;
+            for c in line.as_ref().chars() {
+                if c.is_ascii_digit() {
+                    if first.is_none() {
+                        first = Some(c)
+                    } else {
+                        last = Some(c)
+                    }
+                }
+            }
+            solutions.push((first, last));
+        }
+        Self { solutions }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn day1_examples() {
+        assert_eq!(
+            Day1::solve(&["1abc2"]),
+            Day1 {
+                solutions: vec![(Some('1'), Some('2'))]
+            }
+        );
+        assert_eq!(
+            Day1::solve(&["pqr3stu8vwx"]),
+            Day1 {
+                solutions: vec![(Some('3'), Some('8'))]
+            }
+        );
+        assert_eq!(
+            Day1::solve(&["a1b2c3d4e5f"]),
+            Day1 {
+                solutions: vec![(Some('1'), Some('5'))]
+            }
+        );
+        assert_eq!(
+            Day1::solve(&["treb7uchet"]),
+            Day1 {
+                solutions: vec![(Some('7'), Some('7'))]
+            }
+        );
     }
 }
