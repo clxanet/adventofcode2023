2780f3ad135a28107e1e84c14273efeeb17a16d0
clxa.net <clxanet@pm.me>
2023-12-11 13:30:29 +0200

    change day3 example to vec of all numbers

--- src/day3.rs
+++ src/day3.rs
@@ -76,15 +76,62 @@ mod tests {
     ];
 
     #[test]
-    fn parse_first_number() {
+    fn parse_numbers() {
         let solution = Day3::solve(EXAMPLE);
         assert_eq!(
-            solution.numbers[0],
-            Number {
-                value: 467,
-                x: (0, 2),
-                y: 0,
-            }
+            solution.numbers,
+            vec![
+                Number {
+                    value: 467,
+                    x: (0, 2),
+                    y: 0,
+                },
+                Number {
+                    value: 114,
+                    x: (5, 7),
+                    y: 0,
+                },
+                Number {
+                    value: 35,
+                    x: (2, 3),
+                    y: 2,
+                },
+                Number {
+                    value: 633,
+                    x: (6, 8),
+                    y: 2,
+                },
+                Number {
+                    value: 617,
+                    x: (0, 2),
+                    y: 4,
+                },
+                Number {
+                    value: 58,
+                    x: (7, 8),
+                    y: 5,
+                },
+                Number {
+                    value: 592,
+                    x: (2, 4),
+                    y: 6,
+                },
+                Number {
+                    value: 755,
+                    x: (6, 8),
+                    y: 7,
+                },
+                Number {
+                    value: 664,
+                    x: (1, 3),
+                    y: 9,
+                },
+                Number {
+                    value: 598,
+                    x: (5, 7),
+                    y: 9,
+                }
+            ]
         )
     }
 }
